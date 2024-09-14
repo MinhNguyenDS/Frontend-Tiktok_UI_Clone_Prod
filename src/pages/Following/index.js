f433d6a37ae578770c0a79a18e6a5203fa6df322
Minh Hoàng Nguyễn <nhminh321@gmail.com>
2024-09-14 16:45:17 +0700

    add Following & Live pages

--- src/pages/Following/index.js
+++ src/pages/Following/index.js
@@ -1,5 +1,17 @@
+import classNames from 'classnames/bind';
+import styles from './Following.module.scss';
+import images from '~/assets/images';
+
+const cx = classNames.bind(styles)
+
 function Following() {
-    return <h2>Following page</h2>;
+    return (
+        <div className={cx('error-page')}>
+            <img src={images.error} aria-hidden alt="Error Image" className={cx('error-image')} />
+            <h1> Xin lũiii - Trang này chưa làm 🥹</h1>
+            <p>Trang này chắc dùng để bỏ mấy video cute của ny zô đây</p>
+        </div>
+    );
 }
 
 export default Following;
--- src/pages/Live/index.js
+++ src/pages/Live/index.js
@@ -1,1 +1,17 @@
-export { default } from './Live';
+import classNames from 'classnames/bind';
+import styles from './Live.module.scss';
+import images from '~/assets/images';
+
+const cx = classNames.bind(styles);
+
+function Live() {
+    return (
+        <div className={cx('error-page')}>
+            <img src={images.error2} aria-hidden alt="Error Image" className={cx('error-image')} />
+            <h1> Trang này cũng chưa làm lunn 🥹</h1>
+            <p>Để hôm nào có ý tưởng rồi update sau nha</p>
+        </div>
+    );
+}
+
+export default Live;
